4b5a2f49e95fadbab78b62ab743186c89d0460a9
kaser1997 <kaser.slaiby@gmail.com>
2023-09-07 15:50:42 +0300

    add link css bootstrap in index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { Route } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 
+const appRoutes: Routes = [
+  { path: '' , component:EmployeesComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+  getData() {
+    return this.httpClient.get('http://127.0.0.1/laravel_api/public/api/employees');
+  
+  }
 }
